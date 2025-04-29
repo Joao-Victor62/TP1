46044061dd7803be5c7b1f4e1ba647a44bccb82e
Matheuscamp29 <mdocampello2909@gmail.com>
2025-04-29 11:40:54 -0300

    classe ator so com id e nome

---
+++ aed3/TP1/Ator.java
@@ -0,0 +1,69 @@
+package aed3.TP1;
+
+import aed3.Registro;
+
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+
+public class Ator implements Registro {
+
+    public int id;
+    public String nome;
+
+    public Ator() {
+        this(-1, "");
+    }
+
+    public Ator(String nome) {
+        this(-1, nome);
+    }
+
+    public Ator(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String toString() {
+        return "\nID...............: " + this.id +
+               "\nNome.............: " + this.nome;
+    }
+
+    public byte[] toByteArray() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        dos.writeInt(this.id);
+        dos.writeUTF(this.nome);
+        return baos.toByteArray();
+    }
+
+    public void fromByteArray(byte[] b) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(b);
+        DataInputStream dis = new DataInputStream(bais);
+
+        this.id = dis.readInt();
+        this.nome = dis.readUTF();
+    }
+
+    public String getNomeAtor() {
+        return nome;
+    }
+
+    public int getID() {
+        return id;
+    }
+}
